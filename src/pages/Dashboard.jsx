124ce6a8ed9af066954baefba6027069827d0d3b
Umar Farooq <149324609+umarfarooq6519@users.noreply.github.com>
2024-09-24 09:56:40 +0500

    add dropdown menu, remove signout button from main section

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import { useAuthContext } from "../contexts/AuthContext";
 import "../styles/Dashboard.css";
 
 import SecondaryBtn from "../components/SecondaryBtn";
+import { Dropdown, MenuButton, Menu, MenuItem } from "@mui/joy";
 import UserAvatar from "../components/UserAvatar";
 
 import menu_icon from "/menu_icon.svg";
@@ -25,23 +26,55 @@ function Dashboard() {
 
           <div className='acc_info flex_col_start'>
             <h5 className='name'>
-              Hey there, {f_name}
+              Hey, {f_name}
               <span style={{ fontSize: "18px" }}>👋</span>{" "}
             </h5>
             <span className='greetings'>What's on your mind today?</span>
           </div>
         </div>
 
-        <img src={menu_icon} alt='=' className='icon' />
+        <span className='dropdown'>
+          <Dropdown>
+            <MenuButton variant='plain' color='plain'>
+              <img src={menu_icon} alt='=' className='icon' />
+            </MenuButton>
+            <Menu
+              variant='plain'
+              size='lg'
+              sx={{
+                padding: "0px",
+              }}
+            >
+              <MenuItem
+                variant='soft'
+                color='danger'
+                sx={{
+                  padding: "0px",
+                }}
+              >
+                <button
+                  className='menu-item'
+                  type='button'
+                  onClick={handleSignout}
+                >
+                  <img src={signout_icon} alt='' className='icon' />
+                  Sign Out
+                </button>
+              </MenuItem>
+            </Menu>
+          </Dropdown>
+        </span>
+
+        {/* <img src={menu_icon} alt='=' className='icon' /> */}
       </div>
 
-      <div className='content container'>
+      {/* <div className='content container'>
         <SecondaryBtn
           onClick={handleSignout}
           text='Sign Out'
           icon={<img src={signout_icon} alt='' className='icon' />}
         />
-      </div>
+      </div> */}
     </section>
   );
 }
